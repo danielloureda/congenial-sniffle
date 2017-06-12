f7bec8cdb744e5c7684e65d71d2546febb60e922
Daniel Loureda Arteaga <dloureda@minsait.com>
2017-06-12 14:37:21 +0200

    Project 7 done

---
+++ Project7/Project7/ViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Daniel Loureda Arteaga on 12/6/17.
+//  Copyright © 2017 Dano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var petitions = [[String : String]]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        } else {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+        }
+        
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                let json = JSON(data: data)
+                
+                if json["metadata"]["responseInfo"]["status"].intValue == 200{
+                    //OK
+                    parse(json: json)
+                    return
+                }
+            }
+        }
+        
+        showError()
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    func parse(json: JSON) {
+        for result in json["results"].arrayValue {
+            let title = result["title"].stringValue
+            let body = result["body"].stringValue
+            let sigs = result["signatureCount"].stringValue
+            let obj = ["title": title, "body": body, "sigs": sigs]
+            petitions.append(obj)
+        }
+        
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition["title"]
+        cell.detailTextLabel?.text = petition["body"]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let petition = petitions[indexPath.row]
+        let detailViewController = DetailViewController()
+        detailViewController.detailItem = petition
+        navigationController?.pushViewController(detailViewController, animated: true)
+    }
+}
+
